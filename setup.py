154d26503abbc044a77246789d3787d57bd4d007
Jaap Roes <jroes@leukeleu.nl>
2017-02-27 09:41:17 +0100

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='opvoeden-api-client',
-    version='1.0.0.dev0',
+    version='1.0.0',
     description='Client for Stichting Opvoeden API V1',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     keywords=['stichting', 'opvoeden', 'api', 'client', 'rest'],
